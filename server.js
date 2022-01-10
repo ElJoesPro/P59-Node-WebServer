b0c89c86920078e760ea4dc6311d0c74a446f0bb
Jonathan Guerra <jonathanhttp123@gmail.com>
2022-01-10 07:59:40 -0500

    Puerto para heroku

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const hbs = require('hbs');
 
 require('./hbs/helpers');
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerPartials(__dirname + '/views/parciales');
@@ -28,6 +30,6 @@ hbs.registerHelper('capitalizar', (texo) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 });
